614d02cea4023051a31965c402a6841506665e2c
davidcawork <davidcawork@gmail.com>
2019-06-03 09:21:35 +0200

    update tets_topo_2.py script

--- scripts/test_topo_2.py
+++ scripts/test_topo_2.py
@@ -9,7 +9,7 @@ from mn_wifi.link import wmediumd
 from mn_wifi.wmediumdConnector import interference
 from subprocess import call
 
-def myNetwork():
+def topo():
 
     net = Mininet_wifi(topo=None,
                        build=False,
@@ -17,13 +17,13 @@ def myNetwork():
                        wmediumd_mode=interference,
                        ipBase='10.0.0.0/8')
 
-    info( '*** Adding controller\n' )
+    info( '*** Add controlador\n' )
     c0 = net.addController(name='c0',
                            controller=Controller,
                            protocol='tcp',
                            port=6633)
 
-    info( '*** Add switches/APs\n')
+    info( '*** Add APs\n')
     ap3 = net.addAccessPoint('ap3', cls=OVSKernelAP, ssid='ap3-ssid',
                              channel='1', mode='g', position='772.0,282.0,0')
     ap1 = net.addAccessPoint('ap1', cls=OVSKernelAP, ssid='ap1-ssid',
@@ -31,7 +31,7 @@ def myNetwork():
     ap2 = net.addAccessPoint('ap2', cls=OVSKernelAP, ssid='ap2-ssid',
                              channel='1', mode='g', position='464.0,315.0,0')
 
-    info( '*** Add hosts/stations\n')
+    info( '*** Add estaciones Wifi\n')
     sta1 = net.addStation('sta1', ip='10.0.0.1',
                            position='60.0,423.0,0')
     sta2 = net.addStation('sta2', ip='10.0.0.2',
@@ -39,35 +39,38 @@ def myNetwork():
     sta3 = net.addStation('sta3', ip='10.0.0.3',
                            position='881.0,490.0,0')
 
-    info("*** Configuring Propagation Model\n")
+    info("*** Configuracion del medio\n")
     net.setPropagationModel(model="logDistance", exp=3)
 
-    info("*** Configuring wifi nodes\n")
+    info("*** Configurando los nodos\n")
     net.configureWifiNodes()
 
-    info( '*** Add links\n')
+    info( '*** Add enlaces\n')
     net.addLink(ap1, ap2)
     net.addLink(ap3, ap2)
-
+    
+    #Graficamos con matplotlib la topo
     net.plotGraph(max_x=1000, max_y=1000)
 
-    info( '*** Starting network\n')
+    info( '*** Empieza la topo\n')
     net.build()
-    info( '*** Starting controllers\n')
+
+    info( '*** Controladores UP\n')
     for controller in net.controllers:
         controller.start()
 
-    info( '*** Starting switches/APs\n')
+    info( '*** APs UP\n')
     net.get('ap3').start([c0])
     net.get('ap1').start([c0])
     net.get('ap2').start([c0])
 
     info( '*** Post configure nodes\n')
-
+	
+    #Entramos en matplotlib
     CLI_wifi(net)
     net.stop()
 
 if __name__ == '__main__':
     setLogLevel( 'info' )
-    myNetwork()
+    topo()
 
